732c22d998ddba8ac09d4429d68b448e06765948
Squidmonster64 <marlin-boleros.7r@icloud.com>
2026-08-29 21:07:11 +0800

    Make Fragments capture one-login and auto-route

--- app/static/service-worker.js
+++ app/static/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='fragments-static-v5';
+const CACHE='fragments-static-v6';
 self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{const url=new URL(e.request.url);if(e.request.method==='GET'&&url.origin===self.location.origin&&url.pathname.startsWith('/static/'))e.respondWith(fetch(e.request).then(response=>{if(response.ok){const copy=response.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return response}).catch(()=>caches.match(e.request)))});
